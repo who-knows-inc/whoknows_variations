ac4b593481b37414cd6823b05ccc12b9e6fa7bd0
Jack <jackvwh@hotmail.com>
2024-09-13 12:43:35 +0200

    feat: add About, Login, Register, and Search templates; enhance routing in main.rs; modify dependencies in Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,29 @@ fn index() -> Template {
     Template::render("index", context! { field: "value" })
 }
 
+#[get("/about")]
+fn about() -> Template {
+    Template::render("about", context! { field: "value" })
+}
+
+#[get("/login")]
+fn login() -> Template {
+    Template::render("login", context! { field: "value" })
+}
+
+#[get("/register")]
+fn register() -> Template {
+    Template::render("register", context! { field: "value" })
+}
+
+#[get("/search")]
+fn search() -> Template {
+    Template::render("search", context! { field: "value" })
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, about, login, register, search])
         .attach(Template::fairing())
 }
