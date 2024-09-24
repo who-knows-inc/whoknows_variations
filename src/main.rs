57d3e41c97b7a76eaaaed473f325bcd494f195b2
Jack Hansen <jackvwh@hotmail.com>
2024-09-24 17:50:45 +0200

    Refactor Rocket setup to remove unused config and use default builder for simplicity and improved readability

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,11 @@ async fn rocket() -> _ {
     // Load environment variables from .env file (optional)
     dotenv().ok();
 
-    let figment = rocket::Config::figment().merge((
-        "root",
-        env::var("ROCKET_BASE").expect("ROCKET_BASE must be set"),
-    ));
-
     // Create the database pool
     let pool = get_pool(&env::var("DATABASE_URL").expect("DATABASE_URL must be set")).await;
 
     // Build and return the Rocket instance
-    rocket::custom(figment)
+    rocket::build()
         .attach(Template::fairing())
         .attach(DbConnectionChecker)
         .manage(pool)
