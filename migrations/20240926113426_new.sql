2fc3f4c9b9b8c8ad8f7481e269a8ecfbbaef1b02
Jack Hansen <jackvwh@hotmail.com>
2024-09-26 13:36:29 +0200

    test migration

---
+++ migrations/20240926113426_new.sql
@@ -0,0 +1,3 @@
+-- Add migration script here
+INSERT INTO users (username, email, password) 
+    VALUES ('admin', 'keamonk1@stud.kea.dk', ' $2a$12$Xm.FSBCfvQfGbEC.S5mY2Oc6hWM6B4bCyAWS1rc2BVcoS0rB7PxIC ');
