d088178538da26ce1413fdef0a30bdddc846d542
Jack Hansen <jackvwh@hotmail.com>
2024-11-19 16:55:17 +0100

    Change created_at field in User model to Option type for flexibility in handling user creation time

--- src/models/user.rs
+++ src/models/user.rs
@@ -8,5 +8,5 @@ pub struct User {
     pub username: String,
     pub email: String,
     pub password: String,
-    pub created_at: PrimitiveDateTime,
+    pub created_at: Option<PrimitiveDateTime>, 
 }
